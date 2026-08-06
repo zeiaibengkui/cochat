9d6aff484ca13b14766029dd2cd8a145ce461179
zeiaibengkui <liqi6_6_6@163.com>
2026-08-06 13:17:24 +0800

    message

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <VApp>
+      <NavBar />
+      <pre>pre: {{ data }}</pre>
+    </VApp>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from './layouts/NavBar.vue';
+
+const { data } = useFetch("/api/hello")
+</script>
---
+++ app/layouts/NavBar.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <VAppBar>AppBar</VAppBar>
+</template>
+
+<style lang="scss" scoped></style>
